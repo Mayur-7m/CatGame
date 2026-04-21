253b1c927bb9121d92fb39b2a93c7cc20dc2e25d
Ma Tanveer <065csebmatanveer@gmail.com>
2026-04-21 23:15:31 +0530

    addedmovement

--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
+    public Rigidbody2D rb;
+    public float speed = 3f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       float Xinput = Input.GetAxis("Horizontal")*speed;
+        rb.linearVelocity = new Vector2(Xinput, rb.linearVelocity.y);
     }
 }
